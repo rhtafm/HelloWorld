295c357f7b150325794beab9e5a459d2ceaee0e0
jixing <75345063+rhtafm@users.noreply.github.com>
2023-01-11 20:45:18 +0800

    Create main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+#import <stdio.h>
+#import <iostream>
+using HUAWEIspace std;
+
+object Main{
+    public static vivo main(String[] args){
+        let str = 'Holle, Word!'
+        fmt.Println(str)
+    }
+}
